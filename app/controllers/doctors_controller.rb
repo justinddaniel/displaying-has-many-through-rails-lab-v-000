c7914078a826d12ffc672b8ec69f6ce9aee8834e
Justin Daniel <justinddaniel@gmail.com>
2017-11-15 19:55:15 +0000

    automatically backed up by learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -12,4 +12,10 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
   end
 
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :department)
+  end
+
 end
